fd12b19c4818b96cac347dc7e3cf672b2120c611
Sheli Fine <sheli.fine@post.runi.ac.il>
2024-12-17 22:13:22 +0200

    First test blend

--- Runigram.java
+++ Runigram.java
@@ -158,8 +158,12 @@ public class Runigram {
 	 * values in the two input color.
 	 */
 	public static Color blend(Color c1, Color c2, double alpha) {
-		//// Replace the following statement with your code
-		return null;
+		int red = (int) (alpha * c1.getRed() + (1 - alpha) * c2.getRed());
+		int green = (int) (alpha * c1.getGreen() + (1 - alpha) * c2.getGreen());
+		int blue = (int) (alpha * c1.getBlue() + (1 - alpha) * c2.getBlue());
+
+		Color blendedColor = new Color(red, green, blue);
+		return blendedColor;
 	}
 	
 	/**
@@ -169,8 +173,13 @@ public class Runigram {
 	 * The two images must have the same dimensions.
 	 */
 	public static Color[][] blend(Color[][] image1, Color[][] image2, double alpha) {
-		//// Replace the following statement with your code
-		return null;
+		Color[][] blendedImage = new Color[image1.length][image1[0].length];
+		for (int i = 0; i < blendedImage.length; i++) {
+			for (int j = 0; j < blendedImage.length; j++) {
+				blendedImage[i][j] = blend(image1[i][j], image2[i][j], alpha);
+			}
+		}
+		return blendedImage;
 	}
 
 	/**
